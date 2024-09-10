5448f66307bc870d5f56aa879f66c57cfd92c12e
unknown <pedro_gonsalves@hotmail.com>
2024-09-10 10:36:07 -0300

    add: setup of package

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+from assis.utils import project_root
+
+try:
+    with open(f"{project_root}/readme.md", "r") as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = "A doc generator for data engineering projects."
+
+setup(
+    name='assis',
+    version='0.1',
+    packages=find_packages(),
+    description="A doc generator for data engineering projects.",
+    long_description=long_description,
+    author="Pedro H. Gonçalves",
+    author_email="pedro_gonsalves@hotmail.com",
+    url="https://github.com/pedrohgoncalvess/assis",
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'assis = assis.main:main',
+        ],
+    },
+    install_requires=[
+        "openai",
+        "python-dotenv",
+        "pyyaml"
+    ],
+)
